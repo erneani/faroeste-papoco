52e440d03bd5b200dc684523dc08f8a4999c7a9c
Vinícius Ernani de Carvalho <viniciusernanicarvalho@gmail.com>
2024-08-15 22:14:29 -0300

    feat: make player recoil

--- engine/scenes/entities/player/Player.gd
+++ engine/scenes/entities/player/Player.gd
@@ -2,10 +2,20 @@ extends CharacterBody2D
 
 @export var speed = 400
 @export var rotation_speed = 1.5
-@export var bullet_recoil = 10
+@export var recoil_speed = 4000
+
+var last_char_direction = Vector2(0,0)
+var can_shoot = true
+
+func push_player_back():
+	velocity = -last_char_direction * recoil_speed
+
 
 func get_inputs():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
+
+	if input_direction != Vector2(0,0):
+		last_char_direction = input_direction
 
 	look_at(global_position + input_direction)
 
@@ -14,11 +24,18 @@ func get_inputs():
 	if Input.is_action_pressed("main_action") and input_direction:
 		velocity = Vector2(0, 0)
 	
-	if Input.is_action_just_released("main_action"):
-		velocity = -velocity * bullet_recoil
+	if Input.is_action_just_released("main_action") and can_shoot:
+		can_shoot = false
+		push_player_back()
+		$Timer.start()
 
 
 func _physics_process(delta):
-	get_inputs()
+	if can_shoot:
+		get_inputs()
 
 	move_and_slide()
+
+
+func _on_timer_timeout():
+	can_shoot = true
--- engine/scenes/entities/simple-bullet/SimpleBullet.gd
+++ engine/scenes/entities/simple-bullet/SimpleBullet.gd
@@ -1,3 +1,6 @@
 extends Area2D
 
 @export var speed = 400
+
+func _process(delta):
+	
