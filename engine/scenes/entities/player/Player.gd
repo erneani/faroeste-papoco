b51298d09640c0744b9ad94c5b41418fe5abb1d1
Vinícius Ernani de Carvalho <viniciusernanicarvalho@gmail.com>
2024-08-15 22:52:06 -0300

    feat: shoot bullet

--- engine/scenes/entities/player/Player.gd
+++ engine/scenes/entities/player/Player.gd
@@ -1,7 +1,8 @@
 extends CharacterBody2D
 
+var Bullet = preload("res://scenes/entities/simple-bullet/SimpleBullet.tscn")
+
 @export var speed = 400
-@export var rotation_speed = 1.5
 @export var recoil_speed = 4000
 
 var last_char_direction = Vector2(0,0)
@@ -10,6 +11,18 @@ var can_shoot = true
 func push_player_back():
 	velocity = -last_char_direction * recoil_speed
 
+func instatiate_bullet():
+	var bullet = Bullet.instantiate()
+	bullet.start($Muzzle.global_position, rotation)
+	get_tree().root.add_child(bullet)
+
+func shoot():
+	can_shoot = false
+	push_player_back()
+	instatiate_bullet()
+
+	$Timer.start()
+	
 
 func get_inputs():
 	var input_direction = Input.get_vector("left", "right", "up", "down")
@@ -25,9 +38,7 @@ func get_inputs():
 		velocity = Vector2(0, 0)
 	
 	if Input.is_action_just_released("main_action") and can_shoot:
-		can_shoot = false
-		push_player_back()
-		$Timer.start()
+		shoot()
 
 
 func _physics_process(delta):
--- engine/scenes/entities/simple-bullet/SimpleBullet.gd
+++ engine/scenes/entities/simple-bullet/SimpleBullet.gd
@@ -1,6 +1,14 @@
-extends Area2D
+extends CharacterBody2D
 
-@export var speed = 400
+@export var speed = 1500
 
-func _process(delta):
-	
+func start(_position, _direction):
+	rotation = _direction
+	position = _position
+	velocity = Vector2(speed, 0).rotated(rotation)
+
+func _physics_process(delta):
+	move_and_slide()
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
